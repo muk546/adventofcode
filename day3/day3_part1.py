f30110852e17627147b1e63648b56170a46db530
muk546 <muk546@me.com>
2023-12-05 23:37:29 -0500

    get all symbols

--- day3/day3_part1.py
+++ day3/day3_part1.py
@@ -1,3 +1,4 @@
+import re
 file1 = open('test.txt','r')
 Lines = file1.readlines()
 
@@ -5,41 +6,29 @@ master_control_count = 0
 
 master_dict = {}
 counter = 0
+find_sp_chars_pattern = re.compile(r"[^a-zA-Z\d\.]")
+
 for line in Lines:
     
     #scan for nums and symbols (anything not a '.')
+    index = 0
     clean_line = line.strip()
+    size = len(clean_line)
+    
+    for c in clean_line:
+        result = find_sp_chars_pattern.findall(c)
 
-    list_line = clean_line.split('\.+')
-    print(list_line)
-    master_dict[counter] = list_line
+        if(len(result) != 0):
+            #print(c)
+         print(result[0])
+         master_dict[counter,index] = result[0]
+        index+=1
     counter+=1
 
+for line in Lines:
+   
+
 print(master_dict)
-
-counter2 = 0
-max_size = len(master_dict) - 1
-print(max_size)
-for item in master_dict:
-    print('---------------------')
-
-    if counter2 == 0:
-        #don't look behind!
-        #  
-        print(master_dict[counter2])
-        print(master_dict[counter2 + 1])
-        counter2+=1
-    elif counter2 == max_size:
-        #don't look ahead!
-        #
-        print(master_dict[counter2 - 1])
-        print(master_dict[counter2])
-        counter2+=1
-    else:
-        print(master_dict[counter2 - 1])
-        print(master_dict[counter2])
-        print(master_dict[counter2 + 1])
-        counter2+=1
 
 
 
